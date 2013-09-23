3e0cdb954c8bd6e1030143093bcafb401a791187
Ryan Weaver <ryan@thatsquality.com>
2013-09-23 13:45:03 -0500

    Updating some code that was correct in the script, but had gotten lost in the finish product of the code

--- finish/src/Yoda/EventBundle/Controller/ExceptionController.php
+++ finish/src/Yoda/EventBundle/Controller/ExceptionController.php
@@ -4,26 +4,27 @@ namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\TwigBundle\Controller\ExceptionController as BaseController;
 use Symfony\Component\HttpKernel\Exception\FlattenException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 
 class ExceptionController extends BaseController
 {
     private $exceptionClass;
 
-    public function showAction(FlattenException $exception, DebugLoggerInterface $logger = null, $format = 'html')
+    public function showAction(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null, $format = 'html')
     {
         $this->exceptionClass = $exception->getClass();
 
-        return parent::showAction($exception, $logger, $format);
+        return parent::showAction($request, $exception, $logger, $format);
     }
 
-    protected function findTemplate($templating, $format, $code, $debug)
+    protected function findTemplate(Request $request, $format, $code, $debug)
     {
         if (!$debug && $this->exceptionClass == 'Yoda\EventBundle\Exception\EventNotFoundException') {
             return 'EventBundle:Exception:error404.html.twig';
         }
 
-        return parent::findTemplate($templating, $format, $code, $debug);
+        return parent::findTemplate($request, $format, $code, $debug);
     }
 
 
